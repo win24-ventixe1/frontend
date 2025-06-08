f96c12f36b68f60182f70e2f684b28e07d94dd2f
Jasper <jaspersjohlen@live.se>
2025-06-08 12:21:08 +0200

    Fixat build output directory till dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 👈 detta är viktigt!
+  },
 })
